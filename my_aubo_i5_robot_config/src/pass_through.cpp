70559875f0ed2918011f8db78229bebb93a35f21
myubabe <myubabe@proton.me>
2024-05-19 18:46:25 +0000

    Import code

--- my_aubo_i5_robot_config/src/pass_through.cpp
+++ my_aubo_i5_robot_config/src/pass_through.cpp
@@ -1,4 +1,20 @@
 #include <ros/ros.h>
 #include <iostream>
 
-// PCL specific inc
+// PCL specific includes
+#include <boost/bind.hpp>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_ros/transforms.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filt
