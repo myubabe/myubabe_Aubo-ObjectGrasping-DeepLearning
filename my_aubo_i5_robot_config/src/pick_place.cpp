bcb751a73968e4c30c3c641a1a651a2e6a66b066
myubabe <myubabe@proton.me>
2024-06-01 18:18:59 +0000

    clean up docs

--- my_aubo_i5_robot_config/src/pick_place.cpp
+++ my_aubo_i5_robot_config/src/pick_place.cpp
@@ -15,4 +15,20 @@
  
 void cloud_cb(const visualization_msgs::MarkerArrayConstPtr& grasp_pose_msg)
 {
-  std::cout << "This is the grasp pose:::::::
+  std::cout << "This is the grasp pose:::::::::::::::::::::::::::::::::::::::::::            ";
+  std::cout << grasp_pose_msg->pose.position.x << std::endl;
+}
+*/
+
+void openGripper(trajectory_msgs::JointTrajectory& posture)
+{
+  // BEGIN_SUB_TUTORIAL open_gripper
+  /* Add both finger joints of panda robot. */
+  posture.joint_names.resize(2);
+  posture.joint_names[0] = "gripper_finger1_joint";
+  posture.joint_names[1] = "gripper_finger2_joint";
+
+  /* Set them as open, wide enough for the object to fit. */
+  posture.points.resize(1);
+  posture.points[0].positions.resize(2);
+  posture.points[0].po
